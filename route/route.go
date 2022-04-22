f9ecfbcb69d603b676a2f9488d657aabf624bf07
restuarachman <restuarachman@gmail.com>
2022-04-23 00:40:34 +0700

    disable jwt

--- route/route.go
+++ route/route.go
@@ -1,12 +1,10 @@
 package route
 
 import (
-	"restapi/constants"
 	"restapi/controller"
 	m "restapi/middleware"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 // create a new echo instance
@@ -18,22 +16,22 @@ func New() *echo.Echo {
 	// eAuth := e.Group("")
 	// eAuth.Use(middleware.BasicAuth(m.BasicAuthDB))
 
-	eJwt := e.Group("")
-	eJwt.Use(middleware.JWT([]byte(constants.SECRET_JWT)))
+	// eJwt := e.Group("")
+	// eJwt.Use(middleware.JWT([]byte(constants.SECRET_JWT)))
 	// Route / to handler function
 	e.POST("/login", controller.LoginUserController)
 
-	eJwt.GET("/users", controller.GetUsersController)
-	eJwt.GET("/users/:id", controller.GetUserController)
+	e.GET("/users", controller.GetUsersController)
+	e.GET("/users/:id", controller.GetUserController)
 	e.POST("/users", controller.CreateUserController)
-	eJwt.DELETE("/users/:id", controller.DeleteUserController)
-	eJwt.PUT("/users/:id", controller.UpdateUserController)
+	e.DELETE("/users/:id", controller.DeleteUserController)
+	e.PUT("/users/:id", controller.UpdateUserController)
 
 	e.GET("/books", controller.GetBooksController)
 	e.GET("/books/:id", controller.GetBookController)
-	eJwt.POST("/books", controller.CreateBookController)
-	eJwt.DELETE("/books/:id", controller.DeleteBookController)
-	eJwt.PUT("/books/:id", controller.UpdateBookController)
+	e.POST("/books", controller.CreateBookController)
+	e.DELETE("/books/:id", controller.DeleteBookController)
+	e.PUT("/books/:id", controller.UpdateBookController)
 
 	return e
 }
